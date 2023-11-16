e82a7807dda9b8d90e991c0c13f34acca436ede2
Were Samson Bruno <werefast2000@gmail.com>
2023-11-16 20:36:00 +0000

    create navbar component

---
+++ src/components/component/navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="logo">
+        <img className="WYCO-logo-blk" alt="Wyco logo blk" src="WYCO-logo-blk.png" />
+      </div>
+      <div className="nav-links">
+        <div className="div-wrapper">
+          <div className="text-wrapper">Product</div>
+        </div>
+        <div className="div">
+          <div className="text-wrapper-2">Solutions</div>
+          <img className="akar-icons-chevron" alt="Akar icons chevron" src="akar-icons-chevron-down.svg" />
+        </div>
+        <div className="text-wrapper-3">About</div>
+        <div className="overlap-group">
+          <div className="start-free-trial">Start Free Trial</div>
+        </div>
+        <div className="text-wrapper-4">Pricing</div>
+      </div>
+    </div>
+  );
+};
